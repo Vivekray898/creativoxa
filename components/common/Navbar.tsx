54632f1a0997019690d231c88b43b735fb1ce360
Vivek Ray <vivekray898@hotmail.com>
2026-03-21 20:56:21 +0530

    toggle btton move

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
-import ThemeToggle from "../ThemeToggle";
+import ThemeToggle from "./ThemeToggle";
 
 export default function Navbar() {
   const { resolvedTheme } = useTheme();
